b866993d02469a2c4ebc3bb71c6d0eef54866101
suraj-auth <surajmehto93@gmail.com>
2024-09-01 17:25:09 +0530

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import appimg from "../assets/weather-app.png";
 function Nav() {
   return (
-    <div className="items-center border py-6 border-white flex h-3/10 sm:h-2/10 w-full justify-around ">
+    <div className="items-center py-6 flex h-3/10 sm:h-2/10 w-full justify-around ">
       <div>
         <img className="sm:w-20 w-16" src={appimg} alt="App Image" />
       </div>
